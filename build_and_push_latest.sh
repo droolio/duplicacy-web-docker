17515c70e10d653dd70c0b99c0815d922b1f8cc0
saspus <me@saspus.com>
2023-01-21 14:41:24 -0800

    Add build-and-push script

---
+++ build_and_push_latest.sh
@@ -0,0 +1,11 @@
+git checkout master || exit 1
+
+export DUPLICACY_WEB_VERSION=1.6.3
+
+docker buildx build \
+		--push								\
+		--platform linux/arm/v7,linux/amd64,linux/arm64			\
+		--build-arg DUPLICACY_WEB_VERSION=${DUPLICACY_WEB_VERSION} 	\
+		--tag saspus/duplicacy-web:latest 				\
+		--tag saspus/duplicacy-web:v${DUPLICACY_WEB_VERSION} 		\
+		.
